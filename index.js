e08e3eb23eff14c0bb91f1fbd3bc1de3ae87ebe6
fritzvd <f.vandeventer@gmail.com>
2014-10-09 08:37:52 +0200

    back 2 square one.. taking away webrtc stuff and implementing my own..

--- index.js
+++ index.js
@@ -6,19 +6,9 @@ navigator.getUserMedia  = navigator.getUserMedia ||
                           navigator.msGetUserMedia;
 
 
-
-
-var letterSize = function () {
-  console.log('i have been here')
-  var el = document.getElementById('note');
-  el.style = "font-size: " + window.innerHeight * 0.8 + 'px';
-}
-
-window.onresize = letterSize;
-document.onreadystatechange = letterSize;
-
 var signaltohertz = require('signaltohertz');
 var harrrhmony = require('./harrrhmony');
+var Webrtc = require('simplewebrtc');
 
 var audio, volume, frequencies, frequency, audioContext, analyser, 
     microphone, waveform, amplitude;
@@ -28,20 +18,18 @@ var streamingcb =  function (stream) {
     audioContext = new AudioContext();
     analyser = audioContext.createAnalyser();
     frequencies = new Float32Array(analyser.frequencyBinCount);
-    console.log(analyser)
     amplitude = new Uint8Array(analyser.frequencyBinCount);
 
     volume = audioContext.createGain();
 
-    microphone = audioContext.createMediaStreamSource(stream);
+    microphone = audioContext.createMediaElementSource(el);
     microphone.connect(volume);
     microphone.connect(analyser);
-
     renderFrame();
   };
 
 var errorcb = function (error) {
-    console.log('Did not work', error)
+    console.log('Did not work', error)  
   };
 
 navigator.getUserMedia(
@@ -56,11 +44,54 @@ function renderFrame () {
   analyser.getByteTimeDomainData(amplitude);
 
   frequency = signaltohertz(frequencies);
+  console.log(frequency)
+  // console.log('yahoozie', frequency,  document.querySelector('#mine'));
     //   document.querySelector('#max').innerHTML = max ;
     // document.querySelector('#index').innerHTML = frequency  + '    Hz';
       if (frequency > 20) {
       var note = harrrhmony(frequency);
-        document.querySelector('#note').innerHTML = note.note.note.toString();
+
+        document.querySelector('#mynote').innerHTML = note.note.note.toString();
 
       }
 };
+
+
+
+
+
+// var webrtc = new Webrtc({
+//     // the id/element dom element that will hold "our" video
+//     localVideoEl: 'mine',
+//     // the id/element dom element that will hold remote videos
+//     remoteVideosEl: 'yours',
+//     // immediately ask for camera access
+//     autoRequestMedia: true,
+//     media: {
+//       video: false,
+//       audio: true
+//     },
+//     url: 'http://localhost:8888'
+// });
+
+// var audioM = document.querySelector("#mine");
+ 
+
+// webrtc.on('localStream', function () {
+//   setTimeout(function () {
+//     streamingcb(audioM.children[0]);
+//     window.analyser = analyser
+//   }, 300);
+// });
+
+// webrtc.on('peerStreamAdded', function () {
+
+// })
+  
+
+// // we have to wait until it's ready
+// webrtc.on('readyToCall', function () {
+//   // you can name it anything
+//   webrtc.joinRoom('fritzio');
+// });
+
--- server.js
+++ server.js
@@ -1,21 +1,2 @@
-
-var express = require('express')
-var app = express();
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/'))
-// app.get('/', function(request, response) {
-//     response.send('Hello World!')
-// })
-app.listen(app.get('port'), function() {
-    console.log("Node app is running at localhost:" + app.get('port'))
-})
-
-// var io = require('webrtc.io').listen(8000);
-
-// this is a simple wrapper around socket.io, so you can define your own events
-// like so:
-// io.sockets.on('connection', function(socket) {
-//   socket.on('chat', function(nick, message) {
-//     socket.broadcast.emit('chat', nick, message);
-//   });
-// });
+var PS = require('peer').PeerServer;
+var server = new PS({port: 9000, path: '/'});
